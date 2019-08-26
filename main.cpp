7c11f69b8014a0600910e86bc1e51a10c0924247
Raul Mosquera <raulv.mosquera@gmail.com>
2019-08-25 22:40:04 -0500

    Tex quotes terminates

--- main.cpp
+++ main.cpp
@@ -1,7 +1,51 @@
+#include <fstream>
 #include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <functional>
 
 using namespace std;
 
-int main(int argc, char *argv[]) {
+int main() {
+    string input_line;
+    vector<string> latex;
+    int numberSign=0;
+    string latexChar="";
+    latex.clear();
+    while( getline(cin, input_line) ) {
+        stringstream sep(input_line);
+
+        if(input_line.length() <= 1)
+            break;
+
+        string newLine="";
+        
+        int length = input_line.length();
+
+        for(int i=0; i<length ; i++) {
+            if ( input_line[i] == '"' ) {
+                numberSign++;
+                if ( numberSign % 2 != 0 )
+                    latexChar = "``";
+                else
+                    latexChar = "''";
+                newLine = newLine + latexChar;
+            } else {
+                newLine = newLine + input_line[i];
+            }
+        }
+
+        latex.push_back(newLine);
+    }
+
+    for(auto element: latex) {
+
+        cout << element << "\n";
+    }
+
     return 0;
 }
